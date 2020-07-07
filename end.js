f3e50377c27d1926da1801cf0ab4101109cc7ad1
Vladislav Storozhenko <43917096+VladStorozhenko@users.noreply.github.com>
2020-07-07 18:01:02 +0300

    Update end.js

--- end.js
+++ end.js
@@ -26,7 +26,7 @@ saveHighScore = e => {
   highScores.splice(5);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("/");
+  window.location.assign("./");
 };
 
 saveScoreBtn.addEventListener('click', saveHighScore);
